ef2378e120974c8ff8f3242ca273e48a3bd62633
AMIR <amirjahanbin99@gmail.com>
2020-04-24 18:54:24 +0430

    login page designed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AppRouter from "./routers/AppRouter";
+// import NameForm from "./test/from.test";
 import './App.css';
 
 const App = () => (
     <div>
         <AppRouter/>
+        {/*<NameForm/>*/}
     </div>
 );
 export default App;
+
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,10 +1,35 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import HomePageLink from "../HomePageLink";
+import closeIcon from "../../assets/images/close icon@2x.png";
+import LoginForm from "../LoginForm";
 
-const Login = () => {
-    return (
-        <div>
-            <h3>This is login page!</h3>
-        </div>
-    );
+export default class Login extends React.Component {
+    render() {
+        return (
+            <div className={"main-container"}>
+                <HomePageLink className={"right-container"} id={"home-page-logo"}/>
+                <div className={"middle-container"} id={"login-form-container"}>
+                    <LoginForm className={"form"} id={"login-form"}/>
+                </div>
+                <div className={"left-container"}>
+                    <div className={"login-R-items"}>
+                        <Link to={"/sign-up"}>
+                            عضویت در سایت
+                        </Link>
+                    </div>
+                    <Link to={"/"}>
+                        <img src={closeIcon} alt={closeIcon} className={"close-icon"}/>
+                    </Link>
+                    <div className={"login-R-items"}>
+                        <Link to={"/forget-password-page"}>
+                            فراموش کرده‌ام
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+
 };
-export default Login;
